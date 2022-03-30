5f4f713ae869b3214215a5e8db524ec84a83d9f0
rb-bbw <remo.bergamin@lernende.bbw.ch>
2022-03-30 10:06:53 +0200

    Init

---
+++ src/main/java/ch/bbw/horoscope/HoroscopeApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.horoscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoroscopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoroscopeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bbw/horoscope/HoroscopeApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.horoscope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoroscopeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
